b2427da4d422441fffa75f3d50cac9f4c2d739da
f78947tg <tyler.goyea@student.manchester.ac.uk>
2024-02-01 22:21:27 +0000

    routing proposal

--- frontend/BrewQuest/src/App.tsx
+++ frontend/BrewQuest/src/App.tsx
@@ -1,9 +1,20 @@
 import './App.css';
 
-import Landing from './containers/Landing';
+import {Landing,Login,Register} from "./containers/index";
+import {Routes,Route} from "react-router-dom";
 
 function App() {
-  return <Landing />;
+  return (
+    <>
+    
+  <Routes>
+    <Route path="/" element={<Landing></Landing>}></Route>
+    <Route path="/Login" element={<Login></Login>}></Route>
+    <Route path="/Register" element={<Register></Register>}></Route>
+    
+    </Routes>
+    </>
+  );
 }
 
 export default App;
--- frontend/BrewQuest/src/containers/Landing.tsx
+++ frontend/BrewQuest/src/containers/Landing.tsx
@@ -1,4 +1,5 @@
 //this is the landing page
+import { Link } from 'react-router-dom';
 import '../containers/Landing.css';
 const Landing = () => {
   return (
@@ -7,7 +8,9 @@ const Landing = () => {
         <h1 className='text display-1'>Join Game</h1>
       </button>
       <button type='button' className='btn btn-primary' id='host-btn'>
-        <h2 className='text'>Host Game</h2>
+      <Link to="/Login"><h2 className='text'>Host Game</h2></Link>
+        
+        
       </button>
     </div>
   );
