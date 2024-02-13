cf9740f4918d1a4ad866532bf7d87c958d88bb86
f78947tg <tyler.goyea@student.manchester.ac.uk>
2024-02-13 10:13:26 +0000

    routing structure

--- frontend/BrewQuest/src/App.tsx
+++ frontend/BrewQuest/src/App.tsx
@@ -1,9 +1,22 @@
 import './App.css';
+import {Routes,Route} from "react-router-dom";
 
-import GamePin from './containers/GamePin';
+import {GamePin,Landing,Login,Register,Placeholder} from './containers/index';
+
 
 function App() {
-  return <GamePin />;
+  return (
+    <>
+    <Routes>
+      <Route path="/" element={<Landing></Landing>}></Route>
+      <Route path="/gamepin" element={<GamePin/>}></Route>
+      <Route path="/login" element={<Login/>}></Route>
+      <Route path="/register" element={<Register/>}></Route>
+      <Route path="/placeholder" element={<Placeholder/>}></Route>
+    </Routes>
+
+    </>
+  );
 }
 
 export default App;
--- frontend/BrewQuest/src/containers/Landing.tsx
+++ frontend/BrewQuest/src/containers/Landing.tsx
@@ -1,14 +1,19 @@
 //this is the landing page
 import '../containers/Landing.css';
+import { Link } from "react-router-dom";
 const Landing = () => {
   return (
     <div className='container text-center'>
-      <button className='btn btn-primary' id='join-btn'>
-        <h1 className='text display-1'>Join Game</h1>
-      </button>
+      <Link to="/gamepin" style={{ textDecoration: 'none' }}>
+        <button className='btn btn-primary' id='join-btn'>
+          <h1 className='text display-1'>Join Game</h1>
+        </button>
+      </Link>
+      <Link to="/Login" style={{ textDecoration: 'none' }}>
       <button type='button' className='btn btn-primary' id='host-btn'>
-        <h2 className='text'>Host Game</h2>
+          <h2 className='text'>Host Game</h2>
       </button>
+      </Link>
     </div>
   );
 };
