94c2c76afc816bde6461816b70e4320cb7d5f485
Toby Fung <toby.fung@student.manchester.ac.uk>
2024-02-19 10:35:22 +0000

    added save & exit button, paginator (doesn't work yet), styling

--- frontend/BrewQuest/src/App.tsx
+++ frontend/BrewQuest/src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css';
 
 import QuizEdit from './containers/QuizEdit';
+import GamePin from './containers/GamePin'
 
 function App() {
   return <QuizEdit />;
--- frontend/BrewQuest/src/containers/QuizEdit.tsx
+++ frontend/BrewQuest/src/containers/QuizEdit.tsx
@@ -1,28 +1,44 @@
+import BackButton from '../components/BackButton/BackButton';
+import Button from 'react-bootstrap/Button';
+import Pagination from 'react-bootstrap/Pagination';
 import React from 'react';
 import '../containers/QuizEdit.css';
 
+let active = 1;
+let items: JSX.Element[] = [];
+for (let number = 1; number <= 5; number++) {
+  items.push(
+    <Pagination.Item key={number} active={number === active}>
+      {number}
+    </Pagination.Item>,
+  );
+}
+
 const QuizEditContainer: React.FC = () => {
+  const handleBackButtonClick = () => {
+    // Replace this with actual functionality when other view exists
+    console.log('Going Back');
+  };
+
+  const handleSaveButtonClick = () => {
+    // Replace this with actual functionality when other view exists
+    console.log('Saving Quiz');
+  };
+
   return (
-    <div>
-      <h1 className='text'>Edit Quiz</h1>
-      <div>
-        <div>
-          <button type='button' className='text'>Add New Question</button>
-        </div>
-        <div>
-          <h2>Edit Existing Questions</h2>
-          <div>
-            <div>
-              <div>
-                <h3 className='text'>Question 1</h3>
-                <button className='text'>Edit</button>
-                <button className='text'>Delete</button>
-              </div>
-            </div>
-          </div>
-        </div>
+    <>
+      <div className='header'>
+        <BackButton onClick={handleBackButtonClick}/>
+        <h1 className='title'>Quiz Title</h1>
+        <Button onClick={handleSaveButtonClick}>Save & Exit</Button>
       </div>
-    </div>
+      <div>
+        <Pagination>{items}</Pagination>
+        <br />
+      </div>
+
+
+    </>
   );
 };
 
